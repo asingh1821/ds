faf0cc4a2364bafa1be602e02b0e5b1047f83c31
asingh1821 <kkamit777@gmail.com>
2024-05-04 22:29:19 +0530

    added rotate array code

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 import array.Insert;
-
+import array.Delete;
+import array.RotateArray;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Insert addElement = new Insert();
-        int[] a = {3, 4, 8, 9, 10, 33, 55,32,-12,-6,0};
-        int sizeOfArray = a.length;
-        addElement.insertAtEnd(a, sizeOfArray, 12);
-        for (int j = 0; j < sizeOfArray; j++)
-        {
-            System.out.print(a[j]+" ");
-        }
+      RotateArray reverseFromIndex=new RotateArray();
+      int arr[]={40,13,27,87,95,40,96,71,35,79,68,2,98,3,18,93,53,57,2,81,87,42,66,90,45,20,41,30,32,18,98,72,82,76,10,28,68,57,98,54,87,66,7,84,20,25,29,72,33,30,4,20,71,69,9,16,41,50,97,24,19,46,47,52,22,56,80,89,65,29,42,51,94,1,35,65,25};
+      reverseFromIndex.rotateArray(arr,69,77);
+  for(int i=0;i<arr.length;i++)
+  {
+      System.out.print(arr[i]+" ");
+  }
     }
 }
 //a[]
