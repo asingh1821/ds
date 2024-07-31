7ef43830a425cc2950225fae52608c71125f7e6c
asingh1821 <kkamit777@gmail.com>
2024-08-01 00:55:22 +0530

    second commit

--- src/DemoMain.java
+++ src/DemoMain.java
@@ -1,5 +1,11 @@
+import java.math.BigInteger;
+
 public class DemoMain {
     public static void main(String[] args) {
-
+        int a = 5,fact = 1;
+        for(int i=1; i<=a; i++){
+           fact *= i;
+        }
+      System.out.println(fact);
     }
 }
